ff0daf087bccc7322b67ba08c7b6e7258eb6ba7c
Nedim Akbaş <nedimakbas34@hotmail.com>
2022-12-12 06:55:34 +0300

    Add project files.

---
+++ Project.Ecommerce.Entity/Customer.cs
@@ -0,0 +1,14 @@
+﻿using Project.Ecommerce.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Ecommerce.Entity
+{
+    public class Customer:EntityBase,IEntity
+    {
+        public string Code { get; set; }
+    }
+}
